9ea0a8b836599131b0eccec6db2026cc09ce0be6
sidddddd-18 <siddharth.sadhwani@gmail.com>
2021-03-09 14:45:40 +0530

    final

--- script.js
+++ script.js
@@ -117,6 +117,30 @@ var hoverDistort = new hoverEffect({
     displacementImage: '4.png'
 });
 
+var hoverDistort1 = new hoverEffect({
+    parent: document.querySelector('.wrapper1'),
+    intensity: 0.5,
+    image1: 'SV-6.png',
+    image2: 'SV-1.png',
+    displacementImage: '4.png'
+});
+
+var hoverDistort2 = new hoverEffect({
+    parent: document.querySelector('.wrapper2'),
+    intensity: 0.5,
+    image1: 'TC-3.png',
+    image2: 'TC-1.png',
+    displacementImage: '4.png'
+});
+
+var hoverDistort3 = new hoverEffect({
+    parent: document.querySelector('.wrapper3'),
+    intensity: 0.5,
+    image1: 'bg.png',
+    image2: 'level_bg.png',
+    displacementImage: '4.png',
+    imagesRatio: 1
+});
 
 const images = ["TC-3.png", "SV-1.png", "bg.png"];
 const texts = [["timcoins"], [""], [""]];
@@ -156,13 +180,5 @@ rgbKineticSlider = new rgbKineticSlider({
     textsRgbIntensity: 1,
 });
 
-var counter = 1;
-    setInterval(function(){
-      document.getElementById('radio' + counter).checked = true;
-      counter++;
-      if(counter > 4){
-        counter = 1;
-      }
-    }, 5000);
 
     
